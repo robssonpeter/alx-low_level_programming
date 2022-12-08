cf6a7d011378cb715fd61fdc77cb3eb4d7e89358
Peter Mgembe <peterrobsson@gmail.com>
2022-12-08 14:20:45 -0800

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@ int main(){
   unsigned int tasks = 21; 
   long longer = 35l;
   float temp = 3.4f;
-  printf("Size of a char: %d byte(s)\n", character);
-  printf("Size of an int: %d byte(s)\n", age);
-  printf("Size of a long int: %d byte(s)\n", tasks);
-  printf("Size of a long long: %d byte(s)\n", longer);
-  printf("Size of a float: %d byte(s)\n", temp);
+  printf("Size of a char: %d byte(s)\n", sizeof(character));
+  printf("Size of an int: %d byte(s)\n", sizeof(age));
+  printf("Size of a long int: %d byte(s)\n", sizeof(tasks));
+  printf("Size of a long long: %d byte(s)\n", sizeof(longer));
+  printf("Size of a float: %d byte(s)\n", sizeof(temp));
   return 0;
 }
