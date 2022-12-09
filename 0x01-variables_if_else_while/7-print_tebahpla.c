2c7a55f8c3562f5c3427d8690a0fd59670d8a27d
Peter Mgembe <peterrobsson@gmail.com>
2022-12-09 14:24:20 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * Return: return zero
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'z'; chr >= 'a'; chr--)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
+
