3caf5995a037663b1b416d41c4d998bab2b37a8c
Peter Mgembe <peterrobsson@gmail.com>
2022-12-09 14:18:42 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * Return: return zero
+ */
+int main(void)
+{
+	for(int x = 0; x < 10; x++)
+	{
+		putchar((char) x);
+	}
+	putchar('\n');
+	return (0);
+}
