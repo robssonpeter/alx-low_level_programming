b65274f864c95a0ba276d0ae78bdadce727924bf
Peter Mgembe <peterrobsson@gmail.com>
2022-12-09 13:45:56 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,16 +10,16 @@
  */
 int main(void)
 {
-        char chr;
+	char chr;
 
-        for (chr = 'a'; chr <= 'z'; chr++)
-        {
-                putchar(chr);
-        }
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		putchar(chr);
+	}
 	for (chr = 'A'; chr <= 'Z'; chr++)
-        {
-                putchar(chr);
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
 }
