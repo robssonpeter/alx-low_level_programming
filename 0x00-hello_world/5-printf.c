f7f1eece94a5cb8856fa514e1752de4e4ce9f6b1
Peter Mgembe <peterrobsson@gmail.com>
2022-12-08 18:30:29 -0800

    printf 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main -This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * Return: return zero
+ */
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art\n");
-  return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
