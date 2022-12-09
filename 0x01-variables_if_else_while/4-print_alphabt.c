94a7a5ae959fccaab8bd17412930e43025cd1705
Peter Mgembe <peterrobsson@gmail.com>
2022-12-09 14:04:03 -0800

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main -This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * Return: return zero
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		if(chr == 'q' || chr == 'e')
+		{
+			continue;
+		}
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
+
