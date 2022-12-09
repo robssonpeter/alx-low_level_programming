497fb71e6bd136915c02516c07fc3ce950f27498
Peter Mgembe <peterrobsson@gmail.com>
2022-12-08 18:37:30 -0800

    6 size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+/**
+ * main -This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * Return: return zero
+ */
 int main(void){
-  char character = 'z';
-  int age = 24;
-  unsigned int tasks = 21; 
-  long longer = 35l;
-  float temp = 3.4f;
-  printf("Size of a char: %d byte(s)\n", sizeof(character));
-  printf("Size of an int: %d byte(s)\n", sizeof(age));
-  printf("Size of a long int: %d byte(s)\n", sizeof(tasks));
-  printf("Size of a long long: %d byte(s)\n", sizeof(longer));
-  printf("Size of a float: %d byte(s)\n", sizeof(temp));
-  return 0;
+	char character;
+	int age;
+	unsigned int tasks; 
+	long longer;
+	float temp;
+	printf("Size of a char: %d byte(s)\n", sizeof(character));
+	printf("Size of an int: %d byte(s)\n", sizeof(age));
+	printf("Size of a long int: %d byte(s)\n", sizeof(tasks));
+	printf("Size of a long long: %d byte(s)\n", sizeof(longer));
+	printf("Size of a float: %d byte(s)\n", sizeof(temp));
+	return (0);
 }
