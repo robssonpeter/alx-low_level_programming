beff80b1ea37a0686865f138c355b9b4cf356941
Peter Mgembe <peterrobsson@gmail.com>
2022-12-13 12:02:32 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ * Return: return zero
+ */
+
+int main (void)
+{
+	printf("%s", "_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+THis is the repository for nested loops
