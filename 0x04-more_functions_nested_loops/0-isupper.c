19d39c33a6ba29cd1313d516da83a8ddc8e35d20
Peter Mgembe <peterrobsson@gmail.com>
2022-12-15 15:24:18 -0800

    is upper testing

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+/**
+ * _main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(char c)
+{
+	return (isupper(c));
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More nested loops my guy Swaniker is gonna kill us bro this is pretty intense
