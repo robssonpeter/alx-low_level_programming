65cc102b3efddb44cf03b375abb2783cc36c720c
Peter Mgembe <peterrobsson@gmail.com>
2022-12-19 18:45:10 -0800

    solved some questions

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int swapping values
+ * @a: pointer a
+ * @b: pointer b
+ * Description: swapping values between and b
+ * Return: void
+ * */
+void swap_int(int *a, int *b)
+{
+	int swapper;
+
+	swapper = *a;
+	*a = *b;
+	*b = swapper;
+}
