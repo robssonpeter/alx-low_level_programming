99ba7ac964741086e329eb94d98884ace5fa945f
Peter Mgembe <peterrobsson@gmail.com>
2022-12-19 17:32:52 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 /**
-* reset_to_98 - check the code 
-* Description: reset n to 98
-* @*n: Pointer to n
-* Return: void
+*reset_to_98 - check the code 
+*Description: reset n to 98
+*@*n: Pointer to n
+*Return: void
 */
 void reset_to_98(int *n)
 {
