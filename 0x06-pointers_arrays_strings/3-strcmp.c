2ffb57766c584afdeab8ec78273dcd6c0e80e191
Peter Mgembe <peterrobsson@gmail.com>
2022-12-22 11:18:02 -0800

    3 compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strcmp - compare
+ * @dest: destination
+ * @s1: string 1
+ * @s2: stirng 2
+ * Description: Compare string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
