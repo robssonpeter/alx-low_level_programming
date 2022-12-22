d2b4e03456a92046e74d8d0f61e533d7420fd2fb
Peter Mgembe <peterrobsson@gmail.com>
2022-12-22 05:25:20 -0800

    0 concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+/**
+ * _strcat - concatenation function
+ * Description: Concatenation function 
+ * @dest: Destination
+ * @src: Sources
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return strcat(dest, src);
+}
