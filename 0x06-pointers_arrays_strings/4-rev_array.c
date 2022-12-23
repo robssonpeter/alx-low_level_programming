6191a38a3dd611b89b522d238bdcba65c40d3ac4
Peter Mgembe <peterrobsson@gmail.com>
2022-12-22 18:26:52 -0800

    remaining questions

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,16 +9,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+int i, c;
 
-	for (i = n - 1; i >= 0; i--)
-	{	
-		if (i != n - 1)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);	
-		/*rev[n - 1 - i] = a[i + n - 1];*/
+for (i = 0; (i < (n - 1) / 2); i++)
+	{
+	c = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = c;
 	}
-	printf("\n");	
 }
+
