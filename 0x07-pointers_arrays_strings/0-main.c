a319dea46fd609b05c794d8230f0d25e183020cc
Peter Mgembe <peterrobsson@gmail.com>
2023-01-02 05:45:03 -0800

    trying to fix some errors

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -5,7 +5,7 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
+ * Description: prints buffer in hexa
  * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
