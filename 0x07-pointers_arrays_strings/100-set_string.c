d2bcdd78aaf99176039f0686ef58b473e52281b7
Peter Mgembe <peterrobsson@gmail.com>
2023-01-03 11:19:53 -0800

    trying to fix betty style

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,7 @@
  * set_string - set the value of a pointer to a char
  * @s: pointer to a pointer
  * @to: pointer to a character to
- * Description: set the value of a pointer to a char
+ * 
  * Return: nothing
  */
 
