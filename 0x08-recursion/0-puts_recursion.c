b3068333ffe41796dea7529eae755611a9617933
Peter Mgembe <peterrobsson@gmail.com>
2023-01-04 11:36:39 -0800

    zeroth question

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _puts_recursion - recur for characters
+ * @s: pointer of characters
+ * Description: recur for characters
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*(s));
+	if(*(s+1))
+	{
+		_puts_recursion(s+1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _putchar - putchar function
+ * @c: character
+ * Return: void
+ * Description: Putchar function
+ */
+
+void _putchar(char c)
+{
+	printf("%c", c);
+}
