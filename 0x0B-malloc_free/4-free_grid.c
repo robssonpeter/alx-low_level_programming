b8e6ff632e352bfdb32fb073adee21813e57b08e
Peter Mgembe <peterrobsson@gmail.com>
2023-01-11 10:26:06 -0800

    memory freer

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - memmory freer
+ * @grid: the grid
+ * @height: the height
+ * Description: memory Freer
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+}
