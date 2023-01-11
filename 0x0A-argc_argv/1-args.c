ba9291e0ae410741512885980aeb62419bca4f07
Peter Mgembe <peterrobsson@gmail.com>
2023-01-11 10:48:42 -0800

    fixed some btty style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  * Return: Always (0)
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-        printf("%d\n", argc-1);
+	printf("%d\n", argc - 1);
 	(void) argv;
-        return (0);
+	return (0);
 }
