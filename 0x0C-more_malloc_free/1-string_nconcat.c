8fef7db32eb4edc5ef29e35d194b9b696ba038a0
Peter Mgembe <peterrobsson@gmail.com>
2023-01-12 06:52:45 -0800

    string concatenator

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - function
+ * @s1: first string
+ * @s2: second string
+ * @n: n spaces
+ * Description: string concatenator
+ * Return: pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *allocated;
+	unsigned int length, x, y;
+
+	length = strlen(s1) + n + 1;
+
+	allocated = (char *) malloc(length);
+
+	for (x = 0; x < strlen(s1); x++)
+	{
+		allocated[x] = s1[x];
+	}
+
+	for (y = 0; y < n; y++)
+	{
+		allocated[x + y] = s2[y];
+	}
+	allocated[x + y + 1] = '\0';
+	/*for (x = 0; x < length; x++)
+	{
+		if (x < strlen(s1))
+		{
+			allocated[x] = s1[x];
+		}
+		else if (x == length - 2)
+		{
+			allocated[x] = '\0';
+		}
+		else
+		{
+			allocated[x] = s2[x - (strlen(s1) - 1)];
+		}
+	}*/
+
+	return (allocated);
+}
