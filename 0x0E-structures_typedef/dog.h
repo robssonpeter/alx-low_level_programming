9fbe89ca8c31bda2f7a13586059a60c4d4ae3eb6
Peter Mgembe <peterrobsson@gmail.com>
2023-01-16 08:59:58 -0800

    define a new type of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
+
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
