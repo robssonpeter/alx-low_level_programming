2d0f6f506584d97bc8982efd639c0aae330883c4
Peter Mgembe <peterrobsson@gmail.com>
2023-03-23 20:51:05 -0700

    solved question 0

---
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+int _putchar(char c)
+{
+	return (0);
+}
+
+int _islower(int c)
+{
+
+	return (0);
+}
+
+int _isalpha(int c)
+{
+	return (0);
+}
+
+int _abs(int n)
+{
+	return (0);
+}
+int _isupper(int c)
+{
+	return (0);
+}
+
+int _isdigit(int c)
+{
+	return (0);
+}
+int _strlen(char *s)
+{
+	return (0);
+}
+
+void _puts(char *s)
+{
+
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (char *) memset(s, b, n);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, 2));
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
