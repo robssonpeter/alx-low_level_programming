88b2e89aa3c9cc89ceab08a30ba88c5726d81013
Peter Mgembe <peterrobsson@gmail.com>
2023-07-11 22:04:14 -0700

    solved the second question

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,27 @@
 #include "search_algos.h"
+#include <stdio.h>
 int binary_search(int *array, size_t size, int value)
 {
+	size_t mid, left, right;
 
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (mid = left; mid < right; mid++)
+			printf("%d, ", array[mid]);
+		printf("%d\n", array[mid]);
+
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] > value)
+			right = mid - 1;
+		else
+			left = mid + 1;/**/
+	}
+
+	return (-1);
 }
